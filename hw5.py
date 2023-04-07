434a2d5b293dc3038c09df3b57799b2ca4df6e92
박서현 <126850075+LUNE28136@users.noreply.github.com>
2023-04-07 15:08:26 +0900

    Add files via upload

---
+++ hw5.py
@@ -0,0 +1,23 @@
+def read_single_digit(n):
+    n=int(n)
+    if n==1: return '일'
+    elif n==2: return '이'
+    elif n==3: return '삼'
+    elif n==4: return '사'
+    elif n==5: return '오'
+    elif n==6: return '육'
+    elif n==7: return '칠'
+    elif n==8: return '팔'
+    elif n==9: return '구'
+    else: return '영'
+
+def read_number(n):
+    n=str(n)
+    n1=read_single_digit(n[0])
+    n2=read_single_digit(n[1])
+    n3=read_single_digit(n[2])
+    return n1, n2, n3
+
+num=int(input('세 자리 정수 입력: '))
+read1, read2, read3=read_number(num)
+print(read1, read2, read3)
