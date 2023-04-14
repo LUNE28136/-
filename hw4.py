bd73778d4b9fa1a007817f47eba1c152b8e286c0
박서현 <126850075+LUNE28136@users.noreply.github.com>
2023-04-14 15:38:26 +0900

    Update hw4.py

--- hw4.py
+++ hw4.py
@@ -6,10 +6,10 @@ def print_welcome_message(name):
     msg1=f' Hello {name},'
     msg2=' Welcom to Seoul.'
     nstr = len(msg1) if (len(msg1) > len(msg2)) else len(msg2)
-    rep_char('-',nstr)
+    rep_char('-',nstr+2)
     print(msg1)
     print(msg2)
-    rep_char('-',nstr)
+    rep_char('-',nstr+2)
 
 name=input('Input his/her name: ')
 print_welcome_message(name)
